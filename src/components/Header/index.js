8919719d7b63735197ef3c4ad04e993d45901dd1
Junior <jjalves96@gmail.com>
2020-06-23 09:28:45 -0300

    feat: add component Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styled';
+
+const Header = () => (
+  <S.HeaderWrapper>
+    <S.HeaderTitle>Title</S.HeaderTitle>
+  </S.HeaderWrapper>
+);
+
+export default Header;
