09cd9f34c8ef6eebd72b73450380637ea5ef71df
Junior <jjalves96@gmail.com>
2020-06-23 09:30:05 -0300

    chore: add few details

--- src/components/MenuLinks/index.js
+++ src/components/MenuLinks/index.js
@@ -5,9 +5,15 @@ import * as S from './styled';
 const MenuLinks = () => (
   <S.MenuLinksWrapper>
     <S.MenuLinksList>
-      <S.MenuLinksItem>Home</S.MenuLinksItem>
-      <S.MenuLinksItem>Photos</S.MenuLinksItem>
-      <S.MenuLinksItem>About</S.MenuLinksItem>
+      <S.MenuLinksItem>
+        <S.MenuLinkLink to="/">Home</S.MenuLinkLink>
+      </S.MenuLinksItem>
+      <S.MenuLinksItem>
+        <S.MenuLinkLink to="/categories">Categories</S.MenuLinkLink>
+      </S.MenuLinksItem>
+      <S.MenuLinksItem>
+        <S.MenuLinkLink to="/About">About</S.MenuLinkLink>
+      </S.MenuLinksItem>
     </S.MenuLinksList>
   </S.MenuLinksWrapper>
 );
