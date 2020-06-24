ae87fbc56e233ee245fdb971458560c859a44a68
Junior <jjalves96@gmail.com>
2020-06-24 10:08:00 -0300

    feat: add Navigation component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import HeaderTitle from '../HeaderTitle';
+import Navigation from '../Navigation';
 
 import * as S from './styled';
 
 const Header = () => (
   <S.HeaderWrapper>
     <HeaderTitle />
+    <Navigation />
   </S.HeaderWrapper>
 );
 
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import * as S from './styled';
+
+const Navigation = () => (
+  <S.NavigationWrapper>
+    <h1>menu1</h1>
+    <h1>menu2</h1>
+  </S.NavigationWrapper>
+);
+
+export default Navigation;
