f00d10c25faf5f9e9ee8e8ca1ff0740a64cbf94e
Junior <jjalves96@gmail.com>
2020-06-30 10:44:56 -0300

    chore: Photo Styled experimental

--- src/components/Galery/styled.js
+++ src/components/Galery/styled.js
@@ -1,16 +1,19 @@
 import styled from 'styled-components';
 
 export const GaleryWrapper = styled.section`
+  /* display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  padding: 0;
+  margin: 0 -5px;
+  list-style: none; */
+
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 32px;
   margin: 10rem;
   margin-top: 32px;
   margin-left: 34rem;
-
-  /* padding: 32px; */
-  /* max-width: 1140px; */
-  /* margin: 0 auto; */
 
   img {
     max-width: 100%;
--- src/components/Photo/styled.js
+++ src/components/Photo/styled.js
@@ -12,52 +12,36 @@ export const PhotoWrapper = styled.article`
   box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
   cursor: pointer;
 
-  object-fit: cover;
-  object-position: center center;
-  opacity: 1;
-  /* transition: none 0s ease 0s; */
-  -webkit-transition: all 0.3s !important;
-  -moz-transition: all 0.3s;
-  -webkit-transition: all 0.3s;
-  transition: all 0.3s;
+  /* position: absolute; */
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 11;
 
-  /* width: 100%;
-  border-radius: 2px;
-  -webkit-transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1);
-  -moz-transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1);
-  -ms-transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1);
-  -o-transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1);
-  transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1); */
-
-  -webkit-transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1);
-  -moz-transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1);
-  -ms-transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1);
-  -o-transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1);
-  transition: all 0.8s cubic-bezier(0.19, 1, 0.22, 1);
+  /* 
+  position: relative;
+  overflow: hidden;
+  display: inline-block; */
 
   &:hover img {
-    /* transform: translate(-50%, -50%); */
-    /* padding: 6px 0px 6px 6px; 
-    overflow: hidden; 
-
-    -moz-transform: scale(1.1);
-    -webkit-transform: scale(1.1);
     transform: scale(1.1);
-    transition: all 0.3s !important; */
+    width: 100% !important;
+    -webkit-transition: 0.3s all !important;
+    transition: 0.3s all !important;
+  }
 
-    /* -webkit-transform: scale(1.2);
-    -moz-transform: scale(1.2);
-    -ms-transform: scale(1.2);
-    -o-transform: scale(1.2);
-    transform: scale(1.2); */
-
-    -webkit-transition: all 0.5s ease;
-    -moz-transition: all 0.5s ease;
-    transition: all 0.5s ease;
-    -moz-transform: scale(1.5) rotate(-5deg);
-    -webkit-transform: scale(1.5) rotate(-5deg);
-    -o-transform: scale(1.5) rotate(-5deg);
-    -ms-transform: scale(1.5) rotate(-5deg);
-    transform: scale(1.5) rotate(-5deg);
+  &:after {
+    /* transform: scale(1);
+    
+    /* top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%; */
+    background: #3333339c;
   }
 `;
