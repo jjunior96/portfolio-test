f832cd1e5e5a579ee8e5d6c5f7a4091cf9952b3e
Junior <jjalves96@gmail.com>
2020-07-07 08:57:48 -0300

    chore: add styled into Galery

--- src/components/Galery/index.js
+++ src/components/Galery/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 import Photo from '../Photo';
+import SectionHead from '../SectionHead';
 
 import * as S from './styled';
 
-const Galery = () => (
-  <S.GaleryWrapper>
-    <Photo alt="Tucanos" />
-    <Photo />
-    <Photo />
-    <Photo />
-  </S.GaleryWrapper>
-);
+const Galery = () => {
+  return (
+    <>
+      <SectionHead />
+      <S.GaleryWrapper>
+        <Photo alt="Tucanos" />
+        <Photo />
+        <Photo />
+        <Photo />
+      </S.GaleryWrapper>
+    </>
+  );
+};
 
 export default Galery;
