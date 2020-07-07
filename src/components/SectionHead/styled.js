4f5fd21716f68cd2876828b5ae238c40cfb57d13
Junior <jjalves96@gmail.com>
2020-07-07 08:58:08 -0300

    chore: add few changes

--- src/components/SectionHead/styled.js
+++ src/components/SectionHead/styled.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 export const SectionHead = styled.section`
-  margin-bottom: 16px;
+  /* display: flex; */
+  width: 100%;
+  /* margin-bottom: 16px; */
+  margin: 0 10rem;
+  margin-top: 64px;
+  margin-bottom: 8px;
 `;
 
 export const SectionTitle = styled.h1`
