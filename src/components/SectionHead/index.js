228dfa798a889ad9139179c0fd6fb3439450f6c5
Junior <jjalves96@gmail.com>
2020-07-07 08:24:36 -0300

    feat: SectionHead component

---
+++ src/components/SectionHead/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styled';
+
+const SectionHead = () => (
+  <S.SectionHead>
+    <S.SectionTitle>Title</S.SectionTitle>
+  </S.SectionHead>
+);
+
+export default SectionHead;
---
+++ src/components/SectionHead/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SectionHead = styled.section`
+  margin-bottom: 16px;
+`;
+
+export const SectionTitle = styled.h1`
+  font-size: 2rem;
+`;
